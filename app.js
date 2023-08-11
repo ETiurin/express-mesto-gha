0dd6d365f5593e0525acc4efdb5f41dbdcba05bb
ETiurin <117716801+ETiurin@users.noreply.github.com>
2023-08-11 15:20:56 +0300

    workBeforeReview_pr14_v4.0

--- app.js
+++ app.js
@@ -4,37 +4,40 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const { entryLimiter } = require('./middlewares/rateLimit');
+const rateLimit = require('express-rate-limit');
 const { loginValidation, createUserValidation } = require('./middlewares/validations');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/notFoundError');
 const { login, createUser } = require('./controllers/users');
-const { errorHandler } = require('./middlewares/errorHandler');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
-mongoose.connect(DB_URL);
+mongoose.connect(DB_URL, {});
 
 const userRoute = require('./routes/users');
 const cardRoute = require('./routes/cards');
 
+app.use(rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+}));
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cookieParser());
-app.use(entryLimiter);
 
-app.use('/users', userRoute);
-app.use('/cards', cardRoute);
+app.use('/users', auth, userRoute);
+app.use('/cards', auth, cardRoute);
 app.post('/signin', loginValidation, login);
 app.post('/signup', createUserValidation, createUser);
 
-app.use(auth);
-
-app.use((req, res, next) => next(new NotFoundError('Страница не найдена')));
+app.use('*', (req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден'));
+});
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
-const UnauthorizedError = require('../errors/unauthorizedError');
-const { REGEXP_URL } = require('../utils/constants');
+const { UnauthorizedError } = require('../errors/unauthorizedError');
 
 const userSchema = new mongoose.Schema(
   {
@@ -25,7 +24,7 @@ const userSchema = new mongoose.Schema(
       required: [true, 'Поле "avatar" должно быть заполнено'],
       default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
       validate: {
-        validator: (link) => REGEXP_URL.test(link),
+        validator: (v) => validator.isURL(v),
         message: 'Некорректный URL',
       },
     },
@@ -49,22 +48,20 @@ const userSchema = new mongoose.Schema(
   { versionKey: false },
 );
 
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new UnauthorizedError('Неправильные почта и/или пароль'));
-      }
+userSchema.statics.findUserByCredentials = (email, password) => this.findOne({ email }).select('+password')
+  .then((user) => {
+    if (!user) {
+      return Promise.reject(new UnauthorizedError('Неправильные почта и/или пароль'));
+    }
 
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new UnauthorizedError('Неправильные почта и/или пароль'));
-          }
+    return bcrypt.compare(password, user.password)
+      .then((matched) => {
+        if (!matched) {
+          return Promise.reject(new UnauthorizedError('Неправильные почта и/или пароль'));
+        }
 
-          return user;
-        });
-    });
-};
+        return user;
+      });
+  });
 
 module.exports = mongoose.model('user', userSchema);
